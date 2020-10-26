735fb0c14719ea70d1977fd624fa06b34dfe678c
Iskander U <sasha.u2001@gmail.com>
2020-10-26 18:45:07 -0400

    Made Files for Settings Menu

---
+++ Project/SettingsMenu/SettingsMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+function SettingsMenu(props) {
+    return (
+        <View style = {styles.menu}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    menu: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: "flex-end",
+      alignItems: "center",
+      marginBottom: "15%",
+      marginTop: "4%",
+    },
+});
+
+export default SettingsMenu;
