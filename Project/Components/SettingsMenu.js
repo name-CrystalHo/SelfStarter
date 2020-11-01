d5c488ea2c8a032af5c539ac733a44f65993ec16
Iskander U <sasha.u2001@gmail.com>
2020-11-01 11:40:37 -0500

    Finished settings menu

--- Project/Components/SettingsMenu.js
+++ Project/Components/SettingsMenu.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 
 function SettingsMenu(props) {
     return (
-        <View style = {styles.menu}>
-            
+        <View style = {styles.container}>
+            <View style = {styles.backContainer}>
+                <Image style = {styles.backButton} source={require('../assets/backarrow.png')} />
+            </View>
+            <Text style = {styles.titleText}>Account Settings</Text>
+            <View style = {styles.settingsContainer}>
+                <Text style = {styles.settingsText}>Change Username</Text>
+                <Text style = {styles.settingsText}>Change Password</Text>
+                <Text style = {styles.settingsText}>Change Change Notification Settings</Text>
+            </View>
+            <View style ={styles.logoutButton}>
+                <Text style = {styles.logoutText}>Log Out</Text>
+            </View>
         </View>
+        
     );
-}
+};
 
 const styles = StyleSheet.create({
-    menu: {
+    container: {
       flex: 1,
       backgroundColor: '#fff',
       justifyContent: "flex-end",
@@ -19,6 +31,46 @@ const styles = StyleSheet.create({
       marginBottom: "15%",
       marginTop: "4%",
     },
+    backContainer: {
+        width: '90%',
+        height: 50,
+        justifyContent: "flex-start"
+    },
+    backButton: {
+        height: 20,
+        width: 30
+    },
+    titleText: {
+        fontWeight: 'bold',
+        fontSize: 40,
+        marginTop: '3%'
+    },
+    logoutButton: {
+        width: '45%',
+        height: 50,
+        backgroundColor: "#A9A9B0", 
+        alignItems: "center",
+        justifyContent: "center",
+        position: 'absolute',
+        top: '93%'
+    },
+    logoutText: {
+       color: '#8E1600',
+       fontWeight: 'bold',
+       fontSize: 18
+    },
+    settingsText: {
+        color: '#61D4D4',
+        fontWeight: 'bold',
+        fontSize: 19,
+        marginTop: '10%'
+    },
+    settingsContainer: {
+        width: '85%',
+        height: '75%',
+        alignItems: "center",
+        marginTop: '10%'
+    }
 });
 
 export default SettingsMenu;
