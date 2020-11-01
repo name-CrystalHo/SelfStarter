c44dfb61ddc74dc8904f0409a45fef2d1d6d27d8
Iskander U <sasha.u2001@gmail.com>
2020-11-01 11:12:17 -0500

    Make it mergable with login

--- Project/App.js
+++ Project/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SettingsMenu from './SettingsMenu/SettingsMenu';
+import SettingsMenu from './Components/SettingsMenu';
 
 export default function App() {
   return <SettingsMenu />;
