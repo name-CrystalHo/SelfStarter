d40f8e24441cb4c3204efce4b6bd9d06da126453
Crystal Ho <cth6xmj@virginia.edu>
2020-11-01 19:14:21 -0500

    tested createworkout

--- Project/App.js
+++ Project/App.js
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
 import MainMenuScreen from './Components/MainMenuScreen';
-
+import Workout from './Components/CreateWorkoutMenu';
 export default function App() {
-  return <MainMenuScreen />;
+  return <Workout />;
 }
 
 
